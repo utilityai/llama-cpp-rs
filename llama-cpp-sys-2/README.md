247a2324252ec12538c854ffed4e266389a84f38
marcus <marcus.s.dunn@gmail.com>
2024-01-03 11:09:29 -0800

    added ref to llama cpp 2 in readme and library docs

--- llama-cpp-sys-2/README.md
+++ llama-cpp-sys-2/README.md
@@ -1,3 +1,5 @@
 # llama-cpp-sys
 
 Raw bindings to llama.cpp with cublas support.
+
+See [llama-cpp-2](https://crates.io/crates/llama-cpp-2) for a safe API.
--- llama-cpp-sys-2/src/lib.rs
+++ llama-cpp-sys-2/src/lib.rs
@@ -1,3 +1,5 @@
+//! See [llama-cpp-2](https://crates.io/crates/llama-cpp-2) for a documented and safe API.
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
