c70ceb858ac0496e1372075cf9e52577c5449155
Sepehr <ahmadipoursepehr@gmail.com>
2024-01-16 13:18:51 -0800

    implemented Display for LlamaToken

--- llama-cpp-2/src/token.rs
+++ llama-cpp-2/src/token.rs
@@ -1,6 +1,7 @@
 //! Safe wrappers around `llama_token_data` and `llama_token_data_array`.
 
 use std::fmt::Debug;
+use std::fmt::Display;
 
 pub mod data;
 pub mod data_array;
@@ -9,7 +10,13 @@ pub mod data_array;
 #[repr(transparent)]
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 #[allow(clippy::module_name_repetitions)]
-pub struct LlamaToken(pub llama_cpp_sys_2::llama_token);
+pub struct LlamaToken(  pub llama_cpp_sys_2::llama_token);
+
+impl Display for LlamaToken {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
 impl LlamaToken {
     /// Create a new `LlamaToken` from a i32.
