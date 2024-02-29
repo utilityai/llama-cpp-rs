90e088eed539bc50b321d7754d8ac2b2709414bb
Valentijn Hol <git@hirtol.top>
2024-02-29 23:24:31 +0100

    Remove `-mfpu=neon` as it's enabled by default on 64-bit targets.

--- llama-cpp-sys-2/build.rs
+++ llama-cpp-sys-2/build.rs
@@ -108,7 +108,6 @@ fn main() {
                     build.flag("-mcpu=apple-m1");
                 } else if std::env::var("HOST") == std::env::var("TARGET") {
                     build.flag("-mcpu=native");
-                    build.flag("-mfpu=neon");
                 }
                 build.flag("-pthread");
             }
