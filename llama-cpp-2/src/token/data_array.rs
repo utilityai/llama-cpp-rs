4c2cd79b3f801662ff464e1d09b42ffd6d5eb924
marcus <marcus.s.dunn@gmail.com>
2024-03-07 11:32:33 -0800

    added notes on what each number is centered around

--- llama-cpp-2/src/token/data_array.rs
+++ llama-cpp-2/src/token/data_array.rs
@@ -93,9 +93,9 @@ impl LlamaTokenDataArray {
     ///
     /// * `ctx` - the context to use. May be `None` if you do not care to record the sample timings.
     /// * `last_tokens` - the last tokens in the context.
-    /// * `penalty_last_n` - the number of tokens to consider for the repetition penalty.
-    /// * `penalty_repeat` - the repetition penalty.
-    /// * `penalty_freq` - the frequency penalty.
+    /// * `penalty_last_n` - the number of tokens to consider for the repetition penalty. (1.0 for no penalty)
+    /// * `penalty_repeat` - the repetition penalty. (0.0 for no penalty)
+    /// * `penalty_freq` - the frequency penalty. (0.0 for no penalty)
     ///
     /// # Example
     ///
