31a641b87d1e012b644927dc4729e9632c0772ee
rachiki <ralle.kinkel@gmail.com>
2024-04-12 16:31:42 +0200

    Corrected output format

--- embeddings/src/main.rs
+++ embeddings/src/main.rs
@@ -139,7 +139,7 @@ fn main() -> Result<()> {
         for token in token_line {
             // Attempt to convert token to string and print it; if it fails, print the token instead
             match model.token_to_str(*token) {
-                Ok(token_str) => eprintln!("Token string: {}", token_str),
+                Ok(token_str) => eprintln!(" {} --> {}", token, token_str),
                 Err(e) => {
                     eprintln!("Failed to convert token to string, error: {}", e);
                     eprintln!("Token value: {}", token);
