fdc70c12bfc0c13f9c03d68c07110e59f3c998b5
Britt Lewis <brittlewis12@gmail.com>
2024-09-24 21:56:59 -0400

    Expose flash attention

--- llama-cpp-2/src/context/params.rs
+++ llama-cpp-2/src/context/params.rs
@@ -197,6 +197,36 @@ impl LlamaContextParams {
         self.context_params.n_ubatch
     }
 
+    /// Set the `flash_attention` parameter
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use llama_cpp_2::context::params::LlamaContextParams;
+    /// let params = LlamaContextParams::default()
+    ///     .with_flash_attention(true);
+    /// assert_eq!(params.flash_attention(), true);
+    /// ```
+    #[must_use]
+    pub fn with_flash_attention(mut self, enabled: bool) -> Self {
+        self.context_params.flash_attn = enabled;
+        self
+    }
+
+    /// Get the `flash_attention` parameter
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use llama_cpp_2::context::params::LlamaContextParams;
+    /// let params = LlamaContextParams::default();
+    /// assert_eq!(params.flash_attention(), false);
+    /// ```
+    #[must_use]
+    pub fn flash_attention(&self) -> bool {
+        self.context_params.flash_attn
+    }
+
     /// Set the type of rope scaling.
     ///
     /// # Examples
