21eee351b0a06170cc6fe9e80121bf05da37b047
AsbjornOlling <asbjornolling@gmail.com>
2025-03-24 23:47:52 +0100

    reimplement get_chat_template

--- llama-cpp-2/src/lib.rs
+++ llama-cpp-2/src/lib.rs
@@ -69,9 +69,14 @@ pub enum LLamaCppError {
 /// There was an error while getting the chat template from a model.
 #[derive(Debug, Eq, PartialEq, thiserror::Error)]
 pub enum ChatTemplateError {
-    /// gguf has no chat template
-    #[error("the model has no meta val - returned code {0}")]
-    MissingTemplate(i32),
+    /// gguf has no chat template (by that name)
+    #[error("chat template not found - returned null pointer")]
+    MissingTemplate,
+
+    /// chat template contained a null byte
+    #[error("null byte in string {0}")]
+    NullError(#[from] NulError),
+
     /// The chat template was not valid utf8.
     #[error(transparent)]
     Utf8Error(#[from] std::str::Utf8Error),
